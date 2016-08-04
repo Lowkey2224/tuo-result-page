55b7349beedab08037db2dc30276124afa82ec67
Marcus "Loki" Jenz <jenz@silpion.de>
2016-08-04 13:30:54 +0200

    Added PAge do display Results for mission and mission list

--- src/LokiTuoResultBundle/Controller/DefaultController.php
+++ src/LokiTuoResultBundle/Controller/DefaultController.php
@@ -12,6 +12,28 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('LokiTuoResultBundle:Default:index.html.twig');
+        $missions = $this->getDoctrine()->getRepository('LokiTuoResultBundle:Mission')->findAll();
+        return $this->render('LokiTuoResultBundle:Default:index.html.twig',
+            [
+                'missions' => $missions,
+            ]);
     }
+
+    /**
+     * @Route("/mission/{missionId}", requirements={"missionId":"\d+"}, name="tuo.showmission")
+     */
+    public function showMission($missionId)
+    {
+        $mission = $this->getDoctrine()->getRepository('LokiTuoResultBundle:Mission')->find($missionId);
+        if(!$mission){
+            return $this->createNotFoundException();
+        }
+        $results = $this->getDoctrine()->getRepository('LokiTuoResultBundle:Result')->findBy(['mission'=>$mission]);
+        return $this->render('LokiTuoResultBundle:Default:showMission.html.twig',
+            [
+                'results' => $results
+            ]);
+    }
+
+
 }
