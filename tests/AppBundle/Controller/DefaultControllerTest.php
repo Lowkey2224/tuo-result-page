ccc97e0cb621fe8ccf309c52dd2f06f89d767e74
Marcus "Loki" Jenz <jenz@silpion.de>
2016-08-08 19:53:50 +0200

    added auth to tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -8,11 +8,13 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'Loki',
+            'PHP_AUTH_PW'   => 'wus3ldus3l',
+        ));
 
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 }
--- tests/LokiTuoResultBundle/Controller/DefaultControllerTest.php
+++ tests/LokiTuoResultBundle/Controller/DefaultControllerTest.php
@@ -8,10 +8,13 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'Loki',
+            'PHP_AUTH_PW'   => 'wus3ldus3l',
+        ));
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
