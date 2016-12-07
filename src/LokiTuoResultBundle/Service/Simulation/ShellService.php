58900205bd447a9972ef0ab1c41754474c5b135d
Marcus "Loki" Jenz <jenz@silpion.de>
2016-12-07 11:30:43 +0100

    fixed BatchService removed unused code

--- src/LokiTuoResultBundle/Service/Simulation/ShellService.php
+++ src/LokiTuoResultBundle/Service/Simulation/ShellService.php
@@ -8,8 +8,6 @@
 
 namespace LokiTuoResultBundle\Service\Simulation;
 
-use LokiTuoResultBundle\Entity\OwnedCard;
-use LokiTuoResultBundle\Entity\Player;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
@@ -28,87 +26,9 @@ class ShellService implements SimulationCreatorInterface
 
     public function getSimulation(Simulation $simulation)
     {
-        $result = $this->engine->render('@LokiTuoResult/Simulation/shell_script.sh.twig', [
+        return $this->engine->render('@LokiTuoResult/Simulation/shell_script.sh.twig', [
             'simulation' => $simulation
         ]);
 
-        return $result;
-        $script = "echo \"" . $simulation->getGuild() . " Results 1,3,6-24\"\n";
-        $script .= "echo \"" . $simulation->getGuild() . " Results 1,3,6-24\" > ./result.txt\n";
-        $script .= $this->getMemberCards($simulation->getPlayers());
-        $script .= "\n\n";
-
-
-        $script .= "echo Number of sims pending : ".$simulation->getNumberOfSimulations()."\n";
-
-        $script .= $this->getExecutionLines($simulation);
-
-        return $script;
-    }
-
-    /**
-     * @param Player[] $players
-     * @return string
-     */
-    private function getMemberCards($players)
-    {
-        $string = "";
-        foreach ($players as $player) {
-            $owned = $player->getOwnedCards();
-
-            $string .= "MemberDeck" . $player->getId() . "=\"" . implode(", ", $owned->toArray()) . "\"\n";
-        }
-
-        return $string;
-    }
-
-    /**
-     * @param Simulation $simulation
-     * @return string
-     */
-    private function getExecutionLines(Simulation $simulation)
-    {
-        $players = $simulation->getPlayers();
-        $result = "";
-        foreach ($players as $player) {
-            $result .= $this->getMissionExecutionsForPlayer($player, $simulation);
-        }
-
-        return $result;
-    }
-
-    /**
-     * @param Simulation $simulation
-     * @return string
-     */
-    private function getMissionExecutionsForPlayer(Player $player, Simulation $simulation)
-    {
-
-        $str = "";
-        $now = new \DateTime();
-        $deck = $player->getOwnedCards()->filter(function (OwnedCard $ownedCard) {
-            return $ownedCard->getAmountInDeck() > 0;
-        });
-
-        $now = $now->format('m/d/y/h/i/s');
-        foreach ($simulation->getMissions() as $mission) {
-            $str .= 'echo "member name ' . $player->getName() . '@';
-            $str .= $now . ' against ' . $mission . '"' . "\n";
-            $str .= 'echo "member name ' . $player->getName() . '@';
-            $str .= $now . ' against ' . $mission . '" >> ./' . $simulation->getResultFile() . "\n";
-            $str .= './tuo "' . implode(", ", $deck->toArray()) . '" "';
-            $str .= $mission . '" -o="$MemberDeck' . $player->getId().'"';
-            if (!empty($simulation->getStructures())) {
-                $str.= ' yf "'. implode(", ", $simulation->getStructures()).'"';
-            }
-            if (!empty($simulation->getEnemyStructures())) {
-                $str.= ' ef "'. implode(", ", $simulation->getEnemyStructures()).'"';
-            }
-            $str .= ' -t ' . $simulation->getThreadCount() . ' ';
-            $str .= ' -r ' . $simulation->getSimType() . ' ';
-            $str .= $simulation->getIterations() . ' > ./tempRes.txt' . "\n";
-            $str .= 'tail -1 ./tempRes.txt | head -1 >> ./' . $simulation->getResultFile() . "\n\n";
-        }
-        return $str;
     }
 }
