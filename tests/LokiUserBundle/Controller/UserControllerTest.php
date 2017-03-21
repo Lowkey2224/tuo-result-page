897917380faba92ce48b24a04eeb8667ee655223
Marcus "Loki" Jenz <jenz@silpion.de>
2017-03-21 16:45:30 +0100

    Checkstyle

--- tests/LokiUserBundle/Controller/UserControllerTest.php
+++ tests/LokiUserBundle/Controller/UserControllerTest.php
@@ -2,15 +2,14 @@
 
 namespace LokiUserBundle\Tests\Controller;
 
-use PDepend\Util\Log;
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\DomCrawler\Crawler;
 
 class UserControllerTest extends WebTestCase
 {
-    const USER = "foo";
-    const PASSWORD_CORRECT = "foo";
+    const USER             = 'foo';
+    const PASSWORD_CORRECT = 'foo';
 
     public function testIndex()
     {
@@ -28,7 +27,7 @@ class UserControllerTest extends WebTestCase
 
     public function testLoginFail()
     {
-        $client = $this->loginAs("sadasda", "sadaadsdasads");
+        $client = $this->loginAs('sadasda', 'sadaadsdasads');
         $this->assertContains('Invalid credentials.', $client->getResponse()->getContent());
     }
 
@@ -38,16 +37,15 @@ class UserControllerTest extends WebTestCase
         $this->assertContains('Welcome, '.self::USER, $client->getResponse()->getContent());
         $crawler = $client->request('GET', '/user/');
 
+        $user    = 'user';
+        $promote = 'Promote';
+        $demote  = 'Demote';
 
-        $user = "user";
-        $promote = "Promote";
-        $demote = "Demote";
-
-        $this->assertHasColumn($crawler, $user, "ROLE_USER");
+        $this->assertHasColumn($crawler, $user, 'ROLE_USER');
         $crawler = $this->clicPromoteOrDemotekLink($crawler, $client, $user, $promote);
-        $this->assertHasColumn($crawler, $user, "ROLE_MODERATOR, ROLE_USER");
+        $this->assertHasColumn($crawler, $user, 'ROLE_MODERATOR, ROLE_USER');
         $crawler = $this->clicPromoteOrDemotekLink($crawler, $client, $user, $demote);
-        $this->assertHasColumn($crawler, $user, "ROLE_USER");
+        $this->assertHasColumn($crawler, $user, 'ROLE_USER');
     }
 
     public function testDeactivateActivate()
@@ -56,31 +54,30 @@ class UserControllerTest extends WebTestCase
         $this->assertContains('Welcome, '.self::USER, $client->getResponse()->getContent());
         $crawler = $client->request('GET', '/user/');
 
+        $user       = 'user';
+        $deactivate = 'deactivate';
+        $activate   = 'activate';
 
-        $user = "user";
-        $deactivate = "deactivate";
-        $activate = "activate";
-
-        $this->assertHasColumn($crawler, $user, "active");
+        $this->assertHasColumn($crawler, $user, 'active');
         $crawler = $this->clicPromoteOrDemotekLink($crawler, $client, $user, $deactivate);
-        $this->assertHasColumn($crawler, $user, "inactive");
+        $this->assertHasColumn($crawler, $user, 'inactive');
         $crawler = $this->clicPromoteOrDemotekLink($crawler, $client, $user, $activate);
-        $this->assertHasColumn($crawler, $user, "active");
+        $this->assertHasColumn($crawler, $user, 'active');
     }
-
 
     /**
      * @param string $user
      * @param string $password
+     *
      * @return \Symfony\Bundle\FrameworkBundle\Client
      */
     protected function loginAs($user = self::USER, $password = self::PASSWORD_CORRECT)
     {
-        $client = static::createClient();
+        $client  = static::createClient();
         $crawler = $client->request('GET', '/login');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Log in', $client->getResponse()->getContent());
-        $form = $crawler->selectButton('_submit')->form();
+        $form              = $crawler->selectButton('_submit')->form();
         $form['_username'] = $user;
         $form['_password'] = $password;
         $client->followRedirects();
@@ -88,25 +85,27 @@ class UserControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $client->followRedirects(false);
+
         return $client;
     }
 
     private function assertHasColumn(Crawler $crawler, $user, $roles)
     {
-        $rowXpath = './/tbody/tr[td[normalize-space()="%s"]]';
+        $rowXpath   = './/tbody/tr[td[normalize-space()="%s"]]';
         $rolesXpath = $rowXpath.'/td[normalize-space()="%s"]';
-        $xpath = sprintf($rolesXpath, $user, $roles);
-        $text = $crawler->filterXPath($xpath)->text();
+        $xpath      = sprintf($rolesXpath, $user, $roles);
+        $text       = $crawler->filterXPath($xpath)->text();
         $this->assertEquals($roles, trim($text));
     }
 
     private function clicPromoteOrDemotekLink(Crawler $crawler, Client $client, $user, $linkName)
     {
-        $rowXpath = './/tbody/tr[td[normalize-space()="%s"]]';
+        $rowXpath  = './/tbody/tr[td[normalize-space()="%s"]]';
         $linkXpath = $rowXpath.'/td[contains(normalize-space(),"%s")]/a[normalize-space()="%s"]';
-        $xpath = sprintf($linkXpath, $user, $linkName, $linkName);
-        $link = $crawler->filterXPath($xpath)->link();
+        $xpath     = sprintf($linkXpath, $user, $linkName, $linkName);
+        $link      = $crawler->filterXPath($xpath)->link();
         $client->click($link);
+
         return $client->followRedirect();
     }
 }
