f3801f32670543249e62cc74b253ce410f7e5858
Marcus "Loki" Jenz <jenz@silpion.de>
2017-04-03 08:59:31 +0200

    Added AbstractBaseRepository.php

---
+++ src/LokiTuoResultBundle/Repository/AbstractBaseRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace LokiTuoResultBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+abstract class AbstractBaseRepository extends EntityRepository
+{
+    public function findByIds(array $ids)
+    {
+        return $this->createQueryBuilder('ae')
+            ->where('ae.id in (:ids)')
+            ->setParameter('ids', $ids)
+        ->getQuery()->getResult();
+    }
+}
