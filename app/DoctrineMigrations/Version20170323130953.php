c1def3eef5415a1b4fbc9d065e4b594abeb20965
Marcus "Loki" Jenz <jenz@silpion.de>
2017-04-05 17:20:13 +0200

    Cleaned up Migraton

--- app/DoctrineMigrations/Version20170323130953.php
+++ app/DoctrineMigrations/Version20170323130953.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170323130953 extends AbstractMigration implements ContainerAwareInterface
+class Version20170323130953 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -53,75 +53,4 @@ class Version20170323130953 extends AbstractMigration implements ContainerAwareI
         // Drop Table Guild
         $this->addSql('DROP TABLE guild');
     }
-
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
-
-    private function createGuilds()
-    {
-        $guilds = [];
-        $g      = new Guild();
-        $g->setName('CTP');
-        $g->setId(1);
-        $guilds[1] = $g;
-        $g         = new Guild();
-        $g->setName('CNS');
-        $g->setId(2);
-        $guilds[2] = $g;
-        $g         = new Guild();
-        $g->setName('CTN');
-        $g->setId(3);
-        $guilds[3] = $g;
-        $g         = new Guild();
-        $g->setName('Elapse');
-        $g->setId(4);
-        $guilds[4] = $g;
-        $g         = new Guild();
-        $g->setName('-');
-        $g->setId(5);
-        $guilds[5] = $g;
-        $em        = $this->container->get('doctrine')->getManager();
-        foreach ($guilds as $guild) {
-            $em->persist($guild);
-        }
-        $em->flush();
-
-        return $guilds;
-    }
-
-    /**
-     * @param Guild[] $guilds
-     */
-    private function updatePlayers(array $guilds)
-    {
-        $em      = $this->container->get('doctrine')->getManager();
-        $players = $em->getRepository('LokiTuoResultBundle:Player')->findAll();
-        foreach ($players as $player) {
-            $id = $this->guildMap[$player->getCurrentGuild()];
-            $player->setGuild($guilds[$id]);
-            $em->persist($player);
-        }
-        $em->flush();
-    }
-
-    /**
-     * @param Guild[] $guilds
-     */
-    private function updateResults(array $guilds)
-    {
-        $em      = $this->container->get('doctrine')->getManager();
-        $results = $em->getRepository('LokiTuoResultBundle:Result')->findAll();
-        /** @var Result $result */
-        foreach ($results as $result) {
-            $id = $this->guildMap[$result->getGuild()];
-            $result->setDbGuild($guilds[$id]);
-            $em->persist($result);
-        }
-        $em->flush();
-    }
 }
-
-// Update result set created_at = '2015-12-12 12:00:00' where created_at < '2015-09-01 00:00:00'
-// Update result set updated_at = '2015-12-12 12:00:00' where updated_at < '2015-09-01 00:00:00'
