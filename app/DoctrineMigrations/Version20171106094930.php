070bb444f8c2104bf8b0a473f58463d683384096
Marcus "Loki" Jenz <jenz@silpion.de>
2017-11-06 11:49:11 +0100

    Added missing Migration

---
+++ app/DoctrineMigrations/Version20171106094930.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171106094930 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE 
+          player 
+        ADD 
+          last_api_message VARCHAR(255) DEFAULT NULL, 
+        ADD 
+          last_api_time INT DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE player DROP last_api_message, DROP last_api_time');
+    }
+}
